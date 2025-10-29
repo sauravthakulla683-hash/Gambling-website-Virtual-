1ee8ed5c3d911b5fc9a31a225b3ba4e7c859fbef
Saurabh <sauravthakulla683@gmail.com>
2025-10-29 18:02:35 +0530

    Initial commit

---
+++ Frontend/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+      <div className="flex justi">
+        <h1>Gambling</h1>
+        <div>
+          <li>Games</li>
+          <li>LeaderBoard</li>
+          <li>About</li>
+        </div>
+      </div>
+  );
+};
+
+export default Nav;
